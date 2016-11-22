70829ae05c1a10b2581d963a3d27943b02d4ff7f
mimzzy <Chukwuemeka.onwuka@st.vvu.edu.gh>
2016-11-22 11:39:09 +0000

    Create home.php

---
+++ home.php
@@ -0,0 +1,16 @@
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="css/home.css"/>
+<title>EASTBANK SCHOOL</title>
+</head>
+
+<body>
+<div id="content">
+	<div >
+    	<img src="picture/sch.jpg" width="805" height="508" />
+    </div>
+</div>
+</body>
+</html>
