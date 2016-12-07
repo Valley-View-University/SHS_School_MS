4566f4eea12e578807207333b3ed29a11d34f544
samueladetayo <samuel.adetayo@st.vvu.edu.gh>
2016-12-07 19:30:47 +0100

    rolling

--- View_Students.php
+++ View_Students.php
@@ -19,4 +19,27 @@ if(isset($_GET['rs_id']))
 }
 	
 ?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>EASTBANK SCHOOL</title>
+<link rel="stylesheet" type="text/css" href="css/style_view.css" />
+</head>
+
+<body>
+<div id="style_div" >
+<form method="post">
+<table width="755">
+	<tr>
+    	<td width="190px" style="font-size:18px; color:#006; text-indent:30px;">View Students</td>
+        <td>
+        	<a href="?tag=student_entry"><input type="button" title="Add new student" name="butAdd" value="Add New" id="button-search" /></a>
+        </td>
+        <td>
+        	<input type="text" name="searchtxt" title="Enter name for search " class="search" autocomplete="off"/>
+        </td>
+        <td style="float:right">
+        	<input type="submit" name="btnsearch" value="Search" id="button-search" title="Search Student" />
+        </td>
+    </tr>
 
