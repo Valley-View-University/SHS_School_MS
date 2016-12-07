7403114e34207fc5c44e677fef218430ccad03c0
akanyife-joshua <joshua.akanyife@st.vvu.edu.gh>
2016-12-07 19:21:47 +0000

    update

--- View_Faculties.php
+++ View_Faculties.php
@@ -38,3 +38,44 @@ if(isset($_GET['rs_id']))
 </form>
 </div><!--end of style_div -->
 <br />
+<div id="content-input">
+	<form method="post">
+    <table border="1" width="805px" align="center" cellpadding="3" class="mytable" cellspacing="0">
+        <tr>
+            <th>No</th>
+            <th>Department Name</th>
+            <th width="250px">Note</th>
+            <th colspan="2">Operation</th>
+      	</tr>
+        
+         <?php
+		 $key="";
+	if(isset($_POST['searchtxt']))
+		$key=$_POST['searchtxt'];
+	
+	if($key !="")
+		$sql_sel=mysql_query("SElECT * FROM facuties_tbl WHERE faculties_name  like '%$key%' ");
+	else
+    		$sql_sel=mysql_query("SELECT * FROM facuties_tbl");
+			
+			
+			$i=0;
+    while($row=mysql_fetch_array($sql_sel)){
+    $i++;
+    $color=($i%2==0)?"lightblue":"white";
+    	?>
+      <tr bgcolor="<?php echo $color?>">
+            <td><?php echo $i;?></td>
+            <td><?php echo $row['faculties_name'];?></td>
+            <td><?php echo $row['note'];?></td>
+            <td align="center"><a href="?tag=faculties_entry&opr=upd&rs_id=<?php echo $row['faculties_id'];?>" title="Upate"><img src="picture/update.png" /></a></td>
+            <td align="center"><a href="?tag=view_faculties&opr=del&rs_id=<?php echo $row['faculties_id'];?>" title="Delete"><img src="picture/delete.png" /></a></td>
+        </tr>
+    <?php	
+    }
+    ?>
+   	</table>
+ 	</form>
+</div><!--end of content_input -->
+</body>
+</html>
