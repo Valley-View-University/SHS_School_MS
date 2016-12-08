153aa8ef38c7c40921185665ce6de9a514b28232
solomonyaw <sadeklo@st.vvu.edu.gh>
2016-12-08 21:40:04 +0100

    Check student payment record

---
+++ View_studentpay.php
@@ -0,0 +1,23 @@
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>EASTBANK SCHOOL</title>
+<link rel="stylesheet" type="text/css" href="css/style_view.css" />
+</head>
+
+<body>
+<div id="style_div" >
+<form method="post">
+<table width="755">
+	<tr>
+    	<td width="190px" style="font-size:18px; color:#006; text-indent:30px;">View Student Pay</td>
+        <td>
+        	<input type="text" name="searchtxt" title="Enter name for search " class="search" autocomplete="off"/>
+        </td>
+        <td style="float:right">
+        	<input type="submit" name="btnsearch" value="Search" id="button-search" title="Search Student" />
+        </td>
+    </tr>
+</table>
+</form>
