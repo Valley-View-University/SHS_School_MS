b46a0a6203679bb9e25941e6b7c6dba271a4c6b0
akanyife-joshua <joshua.akanyife@st.vvu.edu.gh>
2016-12-08 19:37:02 +0000

    new

---
+++ test_Scores .php
@@ -0,0 +1,66 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="css/style_view.css" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<div id="style_div" >
+<form method="post">
+<table width="755">
+	<tr>
+    	<td width="190px" style="font-size:18px; color:#006; text-indent:30px;">View Scores</td>
+        <td><a href="?tag=score_entry"><input type="button" title="Add new Scores" name="butAdd" value="Add New" id="button-search" /></a></td>
+        <td><input type="text" name="txtsearch" title="Enter name for search " class="search" autocomplete="off"/></td>
+        <td style="float:right"><input type="submit" name="btnsearch" value="Search" id="button-search" title="Search product" /></td>
+    </tr>
+</table>
+</form>
+</div>
+<br />
+
+<div id="content-input">
+	 <table border="1" width="1300px" align="center" cellpadding="3" class="mytable" cellspacing="0">
+        <tr>
+            <th rowspan="2">No</th>
+            <th rowspan="2">Students Name </th>
+            <th rowspan="2">Sex </th>
+            <th rowspan="2">Date of Birth </th>
+            <th colspan="3">French</th>
+            <th colspan="3">Business studies</th>
+            <th colspan="3">Arts</th>
+            <th colspan="3">Maths</th>
+            <th colspan="3">Econs</th>
+            <th colspan="3">English </th>
+            <th rowspan="2">Note</th>
+            <th colspan="2" rowspan="2" >Operation</th>
+        </tr>
+        
+        <tr>
+        	<th>Mid</th>
+            <th>Fin</th>
+            <th> Total</th>
+            
+            <th>Mid</th>
+            <th>Fin</th>
+            <th> Total</th>
+            
+            <th>Mid</th>
+            <th>Fin</th>
+            <th> Total</th>
+            
+            <th>Mid</th>
+            <th>Fin</th>
+            <th> Total</th>
+            
+            <th>Mid</th>
+            <th>Fin</th>
+            <th> Total</th>
+            
+            <th>Mid</th>
+            <th>Fin</th>
+            <th> Total</th>
+        </tr>
+        
+       
