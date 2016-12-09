12e6b45a5d602c4984f2fb9d39523b4ba280357d
samueladetayo <samuel.adetayo@st.vvu.edu.gh>
2016-12-09 20:17:23 +0100

    movement

---
+++ view_nonstaff.php
@@ -0,0 +1,34 @@
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>EASTBANK SCHOOL</title>
+<link rel="stylesheet" type="text/css" href="css/style_view.css" />
+</head>
+
+<body>
+<div id="style_div" >
+<form method="post">
+<table width="755">
+	<tr>
+    	<td width="190px" style="font-size:18px; color:#006; text-indent:30px;">View Non-Staff</td>
+    </tr>
+</table>
+</form>
+</div><!--- end of style_div -->
+<br />
+<div id="content-input">
+<form method="post">
+
+    <table border="1" width="1050px" align="center" cellpadding="3" Gender="mytable" cellspacing="0">
+        <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Address</th>
+            <th>duty</th>
+            <th>Salary</th>
+            <th>Phone</th>
+           
+        </tr>
+        
