3842033a332437ae043ba799993477670662c759
frankda159 <franklin.victor@st.vvu.edu.gh>
2016-12-09 21:35:14 +0100

    Create view_attendance.php

---
+++ view_attendance.php
@@ -0,0 +1,12 @@
+style type="text/css">
+			
+			th{border-bottom:1px solid #333333;border-left:1px solid #333333;border-top:1px solid #666666; padding-bottom:12px;}
+			td{ border-bottom:1px solid #3F3F3F ;border-left:1px solid #333333;}
+			</style>
+<?php
+        $host = "localhost";
+$user = "root";
+$password = "odunayo07";
+$database  = "assignment";
+ 
+ $db_server = mysql_connect("localhost", "root", "odunayo07");
