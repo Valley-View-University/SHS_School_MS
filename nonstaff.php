294ba01cfeb00bb409a3b2231620f45ac2798b2b
akanyife-joshua <joshua.akanyife@st.vvu.edu.gh>
2016-12-09 20:05:23 +0000

    NEW

---
+++ nonstaff.php
@@ -0,0 +1,19 @@
+<html>
+<title>EAST BANK SHS</title>
+<head>
+</head>
+<body>
+
+   <form method="post">
+  NAME: <input type="text" name="name" title="required"><br/><br/>
+  GENDER:<input type="text" name="gender" title="required"><br/><br/>
+  ADDRESS: <input type="text" name="address" title="required"><br/><br/>
+  DUTY:<input type="text" name="duty" title="required"><br/><br/>
+  SALARY:<input type="text" name="salary"title="required"><br/><br/>
+  PHONE:<input type="text" name="phone" title="required"><br/><br/>
+   <input type = "submit" value = "REGISTER">
+		</form>
+	<form method="post">
+    <a href="?tag=view_nonstaff"><input type="button" name="btn_view" title="View Teachers" value="View Nonstaff" id="button_view" style="width:120px;"  /></a>
+             
+       		</form>	
