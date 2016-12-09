d0e71b33b204ef042a5982d8512db61319def926
solomonyaw <sadeklo@st.vvu.edu.gh>
2016-12-09 21:37:39 +0100

    Update view_attendance.php

--- view_attendance.php
+++ view_attendance.php
@@ -10,3 +10,16 @@ $password = "odunayo07";
 $database  = "assignment";
  
  $db_server = mysql_connect("localhost", "root", "odunayo07");
+  if (!$db_server) die("Unable to connect to MySQL: " . mysql_error());
+  mysql_select_db('assignment')
+    or die("Unable to select database: " . mysql_error()); 
+
+			$sel=mysql_query("SELECT * FROM teachercheck");
+			echo '<table style="width:100%;">';
+			echo '<th align="left">NAME</th><th align="left">DATE  </th><th align="left">TIME </th>';
+			$flag = 0;
+			while($fetch=mysql_fetch_array($sel)){
+			echo '<tr><td>'.$fetch['1'].'</td><td>'.$fetch['2'].'</td><td>'.$fetch['3'].'</td></tr>';
+			 
+			}
+			echo '</table>';
